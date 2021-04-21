9e0970f27b79883688a18bb424670a7a60664074
xCruziX <gamesepixfilter@gmail.com>
2021-04-21 18:53:09 +0200

    Refactor

--- client.js
+++ client.js
@@ -19,53 +19,6 @@ socket.on('disconnect', () => {
 //     process.exit(0);
 // })
 
-// handle the event sent with socket.send()
-socket.on('cmd', async (data, cb) => {
-    console.log(`Message vom server: ` + JSON.stringify(data));
-    const {typ, id, cmd} = data;
-    let answer = {};
-    let error = null; 
-    switch(typ)
-    {
-        case 1: // shutdown
-            {
-                error = win.shutdown();
-            }
-        break;
-        case 2: // restart
-            {
-                error = win.restart();
-            }
-        break;
-        case 3: 
-            {
-                answer = await win.systemInfo();
-            }
-        break;
-        case 4: // Notification
-            {
-                let {title, message} = cmd;
-                if(title) 
-                    title = base64Decode(title);
-                if(message) 
-                    message = base64Decode(message);
-                error = win.notify({title, message});
-            }
-        break;
-        case 5: // Powershell
-            {
-                if(cmd) 
-                    error = win.powershell(base64Decode(cmd))
-            }
-        break;
-    }
-    if (cb && (typeof cb == "function")) {
-        const retValue = {...answer, id,error};
-        console.log(`Return object: ` + JSON.stringify(retValue));
-        cb(retValue);
-    }
-});
-
 function base64Decode(text){
     if(!text || text.length === 0) return;
     return Buffer.from(text, 'base64').toString('utf-8');
@@ -87,7 +40,6 @@ socket.on('notify', async (data, cb) => {
     } catch(err)
     {
         console.log(err);
-        // if(cb) cb(err);
     }
 });
 
